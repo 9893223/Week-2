98d65e5dec4523041b09bc8019fec246e148d283
Ben <bderijk@hotmail.co.nz>
2019-03-06 09:55:46 +1300

    Checking

---
+++ .5202 Week 2 exercises/Task 1/Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello" + " " + name);
+
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your gender?");
+            string gender = Console.ReadLine();
+
+            Console.WriteLine("What is your address?");
+            string address = Console.ReadLine();
+
+            Console.WriteLine(name);
+            Console.WriteLine("Your age is:" + " " + age);
+            Console.WriteLine("Your gender is:" + " " + gender);
+            Console.WriteLine("Your address is:" + " " + address);
+
+            Console.ReadLine();
+        }
+    }
+}
