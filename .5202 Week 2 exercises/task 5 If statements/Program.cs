2ed5533554190f83a87b2c77b44fa3a379ead83f
Ben <bderijk@hotmail.co.nz>
2019-03-07 16:22:08 +1300

    msg

---
+++ .5202 Week 2 exercises/task 5 If statements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5_Is_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a 4-digit pin number:");
+            int pin = int.Parse(Console.ReadLine());
+
+            if (pin >= 1000 && pin <= 9999)
+            {
+                Console.WriteLine("Thank you, please re-enter your 4-digit pin number:");
+                int pin2 = int.Parse(Console.ReadLine());
+                if (pin == pin2)
+                    Console.WriteLine("Pin accepted");
+                if (pin == pin2)
+
+                    Console.WriteLine("Thank you, your pin has been set");
+
+                else
+
+                    Console.WriteLine("“Error! Your PIN numbers did not match. Your PIN was not set”.");
+
+            }
+
+            else
+
+                Console.WriteLine("Your pin did not not meet the requirments.");
+
+            Console.ReadLine();
+
+
+
+
+        
+
+
+
+
+        }
+    }
+}
---
+++ .5202 Week 2 exercises/task 6 If statements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Think of a 3 letter palidrome. Input the first letter and press enter?");
+            string firstletter = Console.ReadLine();
+
+            Console.WriteLine("What is the second letter?");
+            string secondletter = Console.ReadLine();
+
+            Console.WriteLine("What is the third letter?");
+            string thirdletter = Console.ReadLine();
+
+            if (firstletter == thirdletter)
+            {
+
+                if (secondletter == "a" || secondletter == "e" || secondletter == "i" || secondletter == "o" || secondletter == "u")
+                    Console.WriteLine("{0}{1}{2} is a palidrome", firstletter, secondletter, thirdletter);
+
+                else
+                    Console.WriteLine("I dont think that's a real word");
+            }
+            else
+                Console.WriteLine("{0}{1}{2} is not a palidrome", firstletter, secondletter, thirdletter);
+
+
+            Console.WriteLine("Task is finsihed. Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+            
+        }
+    }
+}
